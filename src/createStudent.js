eb56b8b17c34b5e78105099dff85854b78e89ac3
Andrew <andrewmll94@gmail.com>
2022-09-20 17:48:42 -0300

    Conclusão do 5° requisito

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name1) => {
+  const student = {
+    name: '',
+    feedback: '',
+  };
+  student.name = name1;
+  student.feedback = () => 'Eita pessoa boa!';
+  return student;
+};
 
 module.exports = createStudent;
